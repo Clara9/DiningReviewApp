52726b831603165847c607a1018eade275235d7c
anoophariharan <anoop.scoop@gmail.com>
2018-10-11 17:51:27 -0500

    Initial Commit

---
+++ TileData.js
@@ -0,0 +1,18 @@
+/**
+ * The example data is structured as follows:
+ *
+ * import image from 'path/to/image.jpg';
+ * [etc...]
+ *
+ * const tileData = [
+ *   {
+ *     img: image,
+ *     title: 'Image',
+ *     author: 'author',
+ *     cols: 2,
+ *   },
+ *   {
+ *     [etc...]
+ *   },
+ * ];
+ */
